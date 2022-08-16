087adbc475f806bf2a357374e716f51381c9dddd
Rosana Rezende <rezende_rosana@hotmail.com>
2022-08-16 18:20:07 -0300

    adiciona aula 01 da semana 08

---
+++ JAVASCRIPT/semana-05/aula-01/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+const estados = ['Sao Paulo', 'Rio de Janeiro', 'Amazonas', 'Pernambuco', 'Santa Catarina', 'Rio Grande do Norte', 'Sergipe']
+console.log(estados)
---
+++ REACT/semana-08/aula-01/meu-primeiro-projeto-react/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Titulo from './components/Titulo';
+import Titulo2 from './components/titulo2';
+
+function App() {
+  return (
+    <div>
+      <Titulo />
+      <Titulo2 />
+      <p>Sou um parágrafo 2</p>
+    </div>
+  );
+}
+
+export default App;
